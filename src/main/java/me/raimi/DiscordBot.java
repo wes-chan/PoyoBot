2a81b043628ac54dd67de07a549e118c0ae9ee2e
Raimiiu <wesleychann@gmail.com>
2023-10-22 04:45:25 +1100

    make it better

--- src/main/java/me/raimi/DiscordBot.java
+++ src/main/java/me/raimi/DiscordBot.java
@@ -1,2 +1,20 @@
-package me.raimi;public class DiscordBot {
+package me.raimi;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class DiscordBot {
+
+    public static void main(String[] args) throws LoginException {
+
+        JDA bot = JDABuilder.createDefault("MTE2NTMyOTY4MzE2OTE1NzIwMg.GBC4M8.Z8s4EYEwW098ERKitBKrGfvWE-bj7G9IM6gOwk")
+                .setActivity(Activity.playing("Poyo~"))
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .build();
+
+    }
 }
